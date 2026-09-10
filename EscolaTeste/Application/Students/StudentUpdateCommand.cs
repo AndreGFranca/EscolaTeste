91eff4499455861774f343e53573038dc6a741cc
AndreGFranca <andregf3@gmail.com>
2026-09-10 15:50:46 -0300

    adicionando ataulização de aluno

---
+++ EscolaTeste/Application/Students/StudentUpdateCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EscolaTeste.Application.Students
+{
+    public class StudentUpdateCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime? DataNascimento { get; set; } = null;
+    }
+}
