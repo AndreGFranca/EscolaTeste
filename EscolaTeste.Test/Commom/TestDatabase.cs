f533a8ced3c97964109e9e099f70d49af6f125de
AndreGFranca <andregf3@gmail.com>
2026-09-11 23:18:17 -0300

    Adição do projeto de teste com os testes integrados

---
+++ EscolaTeste.Test/Commom/TestDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace EscolaTeste.Test.Commom
+{
+    public class TestDatabase
+    {
+        public static string ConnectionString => ConfigurationManager.ConnectionStrings["EscolaTesteIntegration"].ConnectionString;
+    }
+}
